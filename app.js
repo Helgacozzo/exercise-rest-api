b733859148be43f33140b57accf5143a2cf581f8
helgacozzo <helgacozzo@gmail.com>
2024-01-14 15:21:13 +0100

    Fix/Put-Patch

--- app.js
+++ app.js
@@ -18,14 +18,36 @@ const writeResource = (resourceName, resource) => {
     fs.writeFileSync(path.resolve(`./database/${resourceName}.json`), data);
 }
 
-const generateId = (resourceName, resource) => {
+const generateId = (resourceName) => {
     const resource = readResource(resourceName);
     const ids = resource.map(b => b.id);
-    for(let i=0; i<=ids.length; i++){
-        if(!ids.includes(i)){
+    for (let i = 0; i <= ids.length; i++) {
+        if (!ids.includes(i)) {
             return i;
         }
     }
+}
+
+const getSingleResource = (resourceName, req, res) => {
+    const { id } = req.params;
+    if (id === undefined) {
+        res.status(404).send(`You did not provide an ID`)
+        return;
+    }
+    const resource = readResource(resourceName);
+    let resourceIndex;
+    for (let i = 0; i < resource.length; i++) {
+        const element = resource[i];
+        if (element.id === Number(id)) {
+            indexOfDelete = i;
+            break;
+        }
+    }
+    if (resourceIndex === undefined) {
+        res.status(404).send(`There is no ${resourceName} resource with ID ${id}.`)
+        return [];
+    }
+    return [resource[resourceIndex], resourceIndex];
 }
 
 
@@ -41,25 +63,14 @@ app.get('/books', (req, res) => {
     res.sendFile(path.resolve('./database/books.json'));
 });
 
-app.get('/books/:id', (req, res) => {
-    const { id } = req.params;
-    const books = readResource('books');
-    const book = books.filter(b => b.id === Number(id))[0];
-    if(!book){
-        res.status(404).send(`Book with id ${id} not found.`):
-        return;
-    }
-    res.send(book);
-});
-
-
 app.post('/books', (req, res) => {
     const newBook = req.body;
     let isBookvalid = true;
+    isBookvalid &= Object.keys(newBook).length === 3;
     ['title', 'author', 'year'].forEach((key) => {
         isBookvalid &= value[key] !== undefined;
     });
-    if(!isBookvalid){
+    if (!isBookvalid) {
         res.status(400).send("Books must have title, author and year property");
         return;
     }
@@ -70,4 +81,54 @@ app.post('/books', (req, res) => {
     res.send(newBook);
 });
 
+
+app.get('/books/:id', (req, res) => {
+    const [book] = getSingleResource('books', req, res)
+    res.send(book);
+});
+
+app.put('/books/:id', (req, res) => {
+    const newBook = req.body;
+    let isBookvalid = true;
+    isBookvalid &= Object.keys(newBook).length === 3;
+    ['title', 'author', 'year'].forEach((key) => {
+        isBookvalid &= value[key] !== undefined;
+    });
+    if (!isBookvalid) {
+        res.status(400).send("Books must have title, author and year property");
+        return;
+    }
+    const [, indexToUpdate] = getSingleResource('books', req, res);
+    const books = readResource('books');
+    newBook.id = req.params.id;
+    books[indexToUpdate] = newBook;
+    writeResource('books', books);
+    res.send(newBook);
+});
+
+app.patch('/books/:id', (req, res) => {
+    const newProperties = req.body;
+    let isPropertiesValid = Object.keys(newProperties).length <= 3;
+    Object.keys(newProperties).forEach((key) => {
+        isPropertiesValid &= ['title', 'author', 'year'].includes(key);
+    })
+    if (!isBookvalid) {
+        res.status(400).send("Properties must be 3 only and with name key 'title', 'author' or 'year'.");
+        return;
+    }
+    const [, indexToUpdate] = getSingleResource('books', req, res);
+    const books = readResource('books');
+    books[indexToUpdate] = {...books[indexToUpdate], ...newProperties};
+    writeResource('books', books);
+    res.send(newBook);
+});
+
+app.delete('/books/:id', (req, res) => {
+    const { id } = req.params;
+    const books = readResource('books');
+    const [, indexOfDelete] = getSingleResource('books', req.res);
+    books.splice(indexOfDelete, 1);
+    writeResource('books', books);
+    res.send(`Book with id ${id} deleted correctly.`);
+})
 
